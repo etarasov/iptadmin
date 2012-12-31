0d4b88752b44d3c3777864dd138f72d6b6ec9959
Evgeny Tarasov <etarasov.ekb@gmail.com>
2012-12-31 08:01:47 +0000

    [dk] use parser from iptables-helpers to parse chain name

--- src/IptAdmin/EditChainForm/Parse.hs
+++ src/IptAdmin/EditChainForm/Parse.hs
@@ -3,16 +3,15 @@ module IptAdmin.EditChainForm.Parse where
 
 import Control.Monad
 import Data.Char
+import Iptables.Parser
 import Text.ParserCombinators.Parsec.Char
 import Text.ParserCombinators.Parsec.Combinator
 import Text.ParserCombinators.Parsec.Prim hiding (State(..))
 
--- let's assume that chain names can contain only letters and digits
--- it's not mentioned in iptables man page
 parseChainName :: GenParser Char () String
 parseChainName = do
     spaces <?> ""
-    name <- many1 $ oneOf (['A'..'Z'] ++ ['a'..'z'] ++ ['0'..'9']) `label` "latin letter of digit"
+    name <- chainNameParser `label` "latin letter, digit, '_' or '-'"
     when (all isUpper name) $
         fail "Uppercase letters only is prohibited for chain name"
     spaces
